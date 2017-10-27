4d72cca1994a7a24cfb1beb6e1e2fc7e26d26b9b
MichaelPaul0416 <18357903752m0@sina.cn>
2017-10-28 00:16:44 +0800

    【提交时间】201710280015【说明】构建设计模式项目

---
+++ src/main/java/com/wq/design/model/base/AbstractDuck.java
@@ -0,0 +1,10 @@
+package com.wq.design.model.base;
+
+/**
+ * @author: wangqiang20995
+ * @datetime: 2017/10/28 0:13
+ * @description:鸭子的祖宗类
+ * @Reources:
+ */
+public abstract class AbstractDuck {
+}
